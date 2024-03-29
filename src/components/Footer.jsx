c35046a13e5c67f06d080316e5597383b7ca7851
kntjhnllc <kentjohnliloc@gmail.com>
2024-03-30 03:35:14 +0800

    try fixing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,15 +18,19 @@ const Footer = () => {
         </div>
         <div className="bg-neutral-700 my-5 h-[1px] w-full" />
         <div className="flex md:flex-row flex-col md:items-center justify-between">
-        <p className="font-semibold text-gray text-xs">Copyright @2024 Apple Inc. All rights reserved.</p>
-        <div className="flex">
-            {footerLinks.map((link, i)=>(
-                <p key={link} className="text-xs text-gray mr-5">{link}{' '}
-                {i !== footerLinks.length - 1 && <span className="mx-2">|</span>}
-                </p>
-            
+          <p className="font-semibold text-gray text-xs">
+            Copyright @2024 Apple Inc. All rights reserved.
+          </p>
+          <div className="flex flex-wrap">
+            {footerLinks.map((link, i) => (
+              <p key={link} className="font-semibold text-gray text-xs">
+                {link}{" "}
+                {i !== footerLinks.length - 1 && (
+                  <span className="mx-2"> | </span>
+                )}
+              </p>
             ))}
-        </div>
+          </div>
         </div>
       </div>
     </footer>
