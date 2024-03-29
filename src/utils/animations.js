ab77c133620f08abd8cf6c940a790df842968b75
kntjhnllc <kentjohnliloc@gmail.com>
2024-03-30 03:28:38 +0800

    mobile fixing

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -8,7 +8,7 @@ export const animateWithGsap = (target, animationProps, scrollProps)=> {
         ...animationProps, 
         scrollTrigger:{
             trigger: target,
-            toggleActions: 'restart reverse restart reverse',
+            toggleActions: 'restart none restart reverse',
             start: 'top 85%',
             ...scrollProps,
 
